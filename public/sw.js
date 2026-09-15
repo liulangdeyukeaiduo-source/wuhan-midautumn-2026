ae3b8eda1c00408dcb5b30a3e1e7148520cc566e
liulangdeyukeaiduo-source <liulangdeyukeaiduo@gmail.com>
2026-09-15 11:31:08 +0800

    Bump offline cache for mobile task checklist V3

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wuhan-midautumn-2026-v2";
+const CACHE = "wuhan-midautumn-2026-v3";
 const CORE = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
