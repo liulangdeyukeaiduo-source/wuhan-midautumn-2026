f7f1d0e6d0504d047c1d322b2af98a59f56f0ac6
liulangdeyukeaiduo-source <liulangdeyukeaiduo@gmail.com>
2026-09-15 14:20:57 +0800

    Bump cache for editable prep controls

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wuhan-midautumn-2026-v4";
+const CACHE = "wuhan-midautumn-2026-v5";
 const CORE = ["./", "./index.html", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
